0c40751389b08416809df311046e86feea9e3ab7
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-08-29 18:58:52 -0400

    Update netlify config

--- site/content/_index.md
+++ site/content/_index.md
@@ -1,38 +1,38 @@
 ---
 banner: ただいま新6年生～新1年生大募集中！！まずは体験からどうですか？お申し込み・お問い合わせはお気軽に！！
-image: /img/home-jumbotron.jpg
+image: /images/uploads/home-jumbotron.jpg
 blurb:
   heading: 上郷ノーティボーイズ
   text: 私達は、愛知県豊田市南部にある少年軟式野球チーム・上郷ノーティボーイズです。野球を愛する少年・少女達の夢を育み、軟式野球を正しく指導し、体力向上と礼儀を養成します。また、親友同士の友情と交歓の場を与え、規則正しい明朗な少年・少女を育成することを目的としています。
   gm_name: 加藤松男
-  gm_image: /img/mr-kato.jpg
+  gm_image: /images/uploads/mr-kato.jpg
 schedule:
   - team: A
-    date: 7/4（土）
+    date: 2020-07-04
     time: 8：00～13：00
     description: 通常練習
     location_text: 上郷南グランド
     location_url:
   - team: A
-    date: 7/5（日）
+    date: 2020-07-05
     time: 10：20試合開始
     description: ゲオ杯
     location_text: 船見グランドE面
     location_url:
   - team: A
-    date: 7/5（日）
+    date: 2020-07-05
     time: 13：00～16：00
     description: 通常練習
     location_text: 上郷南グランド
     location_url:
   - team: Jr
-    date: 7/4（土）
+    date: 2020-07-04
     time: 13：00～16：00
     description: 通常練習
     location_text: 上郷南グランド
     location_url:
   - team: Jr
-    date: 7/5（日）
+    date: 2020-07-05
     time: 13：00～16：00
     description: 通常練習
     location_text: 上郷南グランド
@@ -40,7 +40,7 @@ schedule:
 info:
   heading: 仲間を募集中
   text: 野球はとても楽しいスポーツです。随時、仲間を募集してます。お待ちしております。
-  image: /img/recruitment.jpg
+  image: /images/uploads/recruitment.jpg
   link_text: お申し込み・お問い合わせ
   link_url: #
 ---
--- src/js/cms.js
+++ src/js/cms.js
@@ -1,19 +1,20 @@
-import React from "react";
-import CMS from "netlify-cms-app";
+import React from 'react';
+import CMS from 'netlify-cms-app';
+import { ja } from 'netlify-cms-locales';
 
 // Import main site styles as a string to inject into the CMS preview pane
-import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
+import styles from '!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css';
 
-import HomePreview from "./cms-preview-templates/home";
-import PostPreview from "./cms-preview-templates/post";
-import TeamPreview from "./cms-preview-templates/team";
-import ActivitiesPreview from "./cms-preview-templates/activities";
-import LinksPreview from "./cms-preview-templates/links";
+import HomePreview from './cms-preview-templates/home';
+import PostPreview from './cms-preview-templates/post';
+import TeamPreview from './cms-preview-templates/team';
 
 CMS.registerPreviewStyle(styles, { raw: true });
-CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("team", TeamPreview);
-CMS.registerPreviewTemplate("activities", ActivitiesPreview);
-CMS.registerPreviewTemplate("links", LinksPreview);
+CMS.registerPreviewTemplate('home', HomePreview);
+CMS.registerPreviewTemplate('post', PostPreview);
+CMS.registerPreviewTemplate('team', TeamPreview);
+
+// https://www.netlifycms.org/docs/configuration-options/#locale
+CMS.registerLocale('ja', ja);
+
 CMS.init();

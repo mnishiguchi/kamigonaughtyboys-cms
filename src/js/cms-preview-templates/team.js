c490dbb69bd7825f9493b61035ee5cab97a6dc07
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-08-30 16:21:51 -0400

    Remove image field from post and team sections

--- src/js/cms-preview-templates/team.js
+++ src/js/cms-preview-templates/team.js
@@ -1,18 +1,14 @@
-import React from "react";
-import format from "date-fns/format";
+import React from 'react';
+import format from 'date-fns/format';
 
-export default class TeamPreview extends React.Component {
-  render() {
-    const {entry, widgetFor, getAsset} = this.props;
-    let image = getAsset(entry.getIn(["data", "image"]));
-
-    return <div className="mw6 center ph3 pv4">
-      <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
+export default function TeamPreview({ entry, widgetFor }) {
+  return (
+    <div className="mw6 center ph3 pv4">
+      <h1 className="f2 lh-title b mb3">{entry.getIn(['data', 'title'])}</h1>
       <div className="cms mw6">
-        <p>{ entry.getIn(["data", "description"]) }</p>
-        { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
-        { widgetFor("body") }
+        <p>{entry.getIn(['data', 'description'])}</p>
+        {widgetFor('body')}
       </div>
-    </div>;
-  }
+    </div>
+  );
 }
